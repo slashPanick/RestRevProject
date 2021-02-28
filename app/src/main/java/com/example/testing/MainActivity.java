6dd9d995a94938d01cf20b991f2955fd7c757c44
Hunter <hunterm1896@gmail.com>
2021-02-28 19:47:01 -0400

    Initial commit

---
+++ app/src/main/java/com/example/testing/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.testing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button startBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startBtn = findViewById(R.id.startBtn);
+
+        //noinspection Convert2Lambda
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MainPage.class);
+                startActivity(intent);
+            }
+        });
+    }}
---
+++ app/src/main/java/com/example/testing/MainPage.java
@@ -0,0 +1,54 @@
+package com.example.testing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainPage extends AppCompatActivity {
+    ListView storeList;
+    ArrayList<String> stores = new ArrayList<>();
+    ArrayAdapter<String> arrayAdapter;
+    EditText searchEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+
+        storeList = findViewById(R.id.storeList);
+        searchEditText = findViewById(R.id.searchEditText);
+
+        stores.add("McDonalds");
+        stores.add("Burger King");
+        stores.add("Pizza Hut");
+        stores.add("Dominoes");
+        stores.add("Wendys");
+        stores.add("Dairy Queen");
+        stores.add("Burrito Jax");
+        stores.add("Boston Pizza");
+
+        arrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, stores);
+        storeList.setAdapter(arrayAdapter);
+
+        searchEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                arrayAdapter.getFilter().filter(s);
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+}
