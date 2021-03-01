d1c4787e50ce012b6d3418399b55878421917486
Hunter <hunterm1896@gmail.com>
2021-02-28 20:27:53 -0400

    Initial commit

--- app/src/main/java/com/example/RestRevProj/MainActivity.java
+++ app/src/main/java/com/example/RestRevProj/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.testing;
+package com.example.RestRevProj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- app/src/test/java/com/example/RestRevProj/ExampleUnitTest.java
+++ app/src/test/java/com/example/RestRevProj/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.testing;
+package com.example.RestRevProj;
 
 import org.junit.Test;
 
